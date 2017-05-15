bce3b3e627f676b511ad33f847046da7347347c1
Kaivan Akhavan Farahani <keyvanaf@users.noreply.github.com>
2017-05-14 21:46:12 -0700

    Add files via upload

---
+++ Blacklane/Scripts/App.js
@@ -0,0 +1,197 @@
+﻿var App=(function ($) {
+    function getValue($el) {
+        return $el.is('[type="checkbox"]') ? $el.prop('checked') :
+            $el.is('[type="radio"]') ? !!$('[name="' + $el.attr('name') + '"]:checked').length :
+                $el.is('select[multiple]') ? ($el.val() || []).length :
+                    $el.val()
+    }
+    function deferredProxy(result) {
+        //is promise
+        if (result && result.result && result.result.then) {
+            return result.result;
+        }
+        var deferred = $.Deferred();
+        deferred.resolve(result);
+        return deferred.promise();
+    }
+    var app = {
+
+        initForm: function () {
+            //Prepare Form
+
+            $(".locationpicker input").placepicker();
+
+            $('input[type=date]').pickadate({
+                format: "dddd d  mmmm, yyyy",
+                min: true,
+                formatSubmit: 'yyyy-mm-dd',
+                hiddenPrefix: 'booking_request[',
+                hiddenSuffix: ']',
+                clear: ''
+
+            }).each(function () {
+                var picker = $(this).pickadate('picker');
+                picker.set('select', new Date());
+
+            });
+
+            $('input[type=time]').pickatime({
+                format: "HH:i (h:i A)",
+                interval: 5,
+                formatSubmit: 'HH:i',
+                hiddenPrefix: 'booking_request[',
+                hiddenSuffix: ']',
+                //clear: '',
+                close: 'close'
+            }).each(function () {
+                var picker = $(this).pickatime('picker');
+                picker.set('select', new Date());
+            });
+
+            //cheng request in one-way
+            var frm = $('#new_booking_request').validator().on('submit', function (e) {
+                if (!$('.has-error').length)//bug:don't current work (!e.originalEvent.defaultPrevented)
+                {
+                    if ($('#twoWay').hasClass('active'))
+                        return;
+                    //remove return fields
+                    var newfrm = frm.clone().appendTo(document.body);
+                    newfrm.find('fieldset.hide input').remove();
+                    newfrm.submit();
+                    e.preventDefault();
+                    e.stopPropagation();
+                    return false;
+                }
+            });
+        },
+
+        initValidation: function () {
+
+            //form prepare validation
+            $.fn.validator.Constructor.INPUT_SELECTOR = '#oneWay input.mustCheck,fieldset.hide.active input.mustCheck'
+
+            $('.frm-transition-header button').on('show.bs.tab', function (e) {
+                setTimeout(function () { $('form').validator('update') });
+            })
+
+
+            //Add Custome Validator
+            var geocoder = new google.maps.Geocoder();
+            $.extend($.fn.validator.Constructor.VALIDATORS, {
+                //validate address with google map api
+                hasaddressstreetnumber: function ($el) {
+                    var deferred = $.Deferred();
+                    geocoder.geocode({ address: getValue($el) }, function (results, status) {
+                        if (status === google.maps.GeocoderStatus.OK) {
+                            if (results[0].types[0] === "street_address")
+                                deferred.resolve()
+                            else
+                                deferred.resolve({ key: "hasaddressstreetnumber", result: "Addresses without a street number, or too generic should not be accepted" });
+                        } else {
+                            deferred.resolve({ key: "hasaddressstreetnumber", result: "http Error:" + status });
+                        }
+                    });
+
+                    return deferred.promise()
+                },
+                //validate Date Bigger than
+                'datebigger': function ($el) {
+                    var target = $el.attr('data-datebigger');
+                    var val1 = $el.val();
+                    if (val1)
+                        val1 = new Date(val1);
+                    else
+                        return;
+                    var val2 = $(target).val();
+                    if (val2)
+                        val2 = new Date(val2);
+                    else
+                        return;
+                    return val1 < val2 && "'" + $(target)[0].title + "' bigger than '" + $el[0].title + "'";
+                }
+            });
+
+
+            //bug:Validator don't support Custom remote validator
+            //solution:chenge this part for support that
+            $.fn.validator.Constructor.prototype.runValidators = function ($el) {
+                var errors = []
+                var deferred = $.Deferred();
+
+                $el.data('bs.validator.deferred') && $el.data('bs.validator.deferred').reject()
+                $el.data('bs.validator.deferred', deferred)
+
+                function getValidatorSpecificError(key) {
+                    return $el.attr('data-' + key + '-error')
+                }
+
+                function getValidityStateError() {
+                    var validity = $el[0].validity
+                    return validity.typeMismatch ? $el.attr('data-type-error')
+                        : validity.patternMismatch ? $el.attr('data-pattern-error')
+                            : validity.stepMismatch ? $el.attr('data-step-error')
+                                : validity.rangeOverflow ? $el.attr('data-max-error')
+                                    : validity.rangeUnderflow ? $el.attr('data-min-error')
+                                        : validity.valueMissing ? $el.attr('data-required-error')
+                                            : null
+                }
+
+                function getGenericError() {
+                    return $el.attr('data-error')
+                }
+
+                function getErrorMessage(key) {
+                    return getValidatorSpecificError(key)
+                        || getValidityStateError()
+                        || getGenericError()
+                }
+                function handleErrorOnPromise(func) {
+                    return function (error) {
+                        if (error && error.result) {
+                            error = getErrorMessage(error.key) || error.result
+                            !~errors.indexOf(error) && errors.push(error);
+                        }
+                        return func();
+                    }
+                }
+                var promise = deferred.promise();
+                deferred.resolve();
+                $.each(this.validators, $.proxy(function (key, validator) {
+                    var $this = this;
+                    promise = promise.then(handleErrorOnPromise(function () {
+                        if (!errors.length && (getValue($el) || $el.attr('required')) &&
+                            ($el.attr('data-' + key) !== undefined || key == 'native')) {
+                            return deferredProxy({ key: key, result: validator.call($this, $el) });
+                        }
+
+                        return deferredProxy();
+
+                    }));
+                }, this))
+
+                deferred = $.Deferred();
+                var $this = this;
+                promise.then(handleErrorOnPromise(function () {
+                    if (!errors.length && getValue($el) && $el.attr('data-remote')) {
+                        $this.defer($el, function () {
+                            var data = {}
+                            data[$el.attr('name')] = getValue($el)
+                            $.get($el.attr('data-remote'), data)
+                                .fail(function (jqXHR, textStatus, error) { errors.push(getErrorMessage('remote') || error) })
+                                .always(function () { deferred.resolve(errors) })
+                        })
+                    } else deferred.resolve(errors)
+                }));
+
+
+
+                return deferred.promise()
+            };
+        },
+
+    };
+
+
+    return app;
+
+})(jQuery);
---
+++ Blacklane/Test/units.js
@@ -0,0 +1,110 @@
+﻿/* ==========================================================================
+   App tests
+   ========================================================================== */
+
+var $template = '                                                                                                                           \
+                                                                                                                                            \
+    <form name="booking_request_form" id="new_booking_request" data-toggle="validator" role="form"                                          \
+          action="https://www.blacklane.com/en/booking_requests/transfers/new" accept-charset="UTF-8" method="get">                         \
+        <input name="utf8" type="hidden" value="✓">                                                                                        \
+        <div class="col-sm-6 input locationpicker form-group">                                                                              \
+            <input class="mustCheck form-control" type="text" id="pickup" name="booking_request[pickup]"                                    \
+                   title="" data-map-container-id="dialogMap1" required data-hasaddressstreetnumber="true">                                 \
+            <div class="help-block with-errors"></div>                                                                                      \
+        </div>                                                                                                                              \
+        <div class="col-sm-6 input locationpicker form-group">                                                                              \
+            <input class="mustCheck form-control" type="text" id="dropoff" name="booking_request[dropoff]"                                  \
+                   title="" data-map-container-id="dialogMap2" required data-hasaddressstreetnumber="true">                                 \
+            <div class="help-block with-errors"></div>                                                                                      \
+        </div>                                                                                                                              \
+        <div class="col-sm-6 input form-group">                                                                                             \
+            <input class="mustCheck datepicker form-control" title="" id="at_date" type="date" name="at_date" required>                     \
+            <div class="help-block with-errors"></div>                                                                                      \
+        </div>                                                                                                                              \
+        <div class="col-sm-6 input form-group">                                                                                             \
+            <input class="mustCheck timepicker form-control" type="time" id="at_time" name="at_time" required>                              \
+            <div class="help-block with-errors"></div>                                                                                      \
+        </div>                                                                                                                              \
+        <div class="col-sm-6 input locationpicker form-group">                                                                              \
+            <input class="mustCheck form-control" type="text" id="return_pickup" name="booking_request[return_pickup]"                      \
+                   title="" data-map-container-id="dialogMap3" required data-hasaddressstreetnumber="true">                                 \
+            <div class="help-block with-errors"></div>                                                                                      \
+        </div>                                                                                                                              \
+        <div class="col-sm-6 input locationpicker form-group">                                                                              \
+            <input class="mustCheck form-control" type="text" id="return_dropoff" name="booking_request[return_dropoff]"                    \
+                   title="" data-map-container-id="dialogMap4" required data-hasaddressstreetnumber="true">                                 \
+            <div class="help-block with-errors"></div>                                                                                      \
+        </div>                                                                                                                              \
+        <div class="col-sm-6 input form-group">                                                                                             \
+            <input class="mustCheck datepicker form-control" title="" type="date" id="return_at_date"                                       \
+                   name="return_at_date" required data-datebigger="#at_date">                                                               \
+            <div class="help-block with-errors"></div>                                                                                      \
+        </div>                                                                                                                              \
+        <div class="col-sm-6 input form-group">                                                                                             \
+            <input class="mustCheck timepicker form-control" type="time" id="return_at_time" name="return_at_time" required>                \
+            <div class="help-block with-errors"></div>                                                                                      \
+        </div>                                                                                                                              \
+    </form>                                                                                                                                 \
+                                                                                                                                            \
+                                                                                                                                            \
+', $DOM =$('#qunit-fixture');
+
+
+QUnit.module('AppTest setup', {
+    beforeEach: function () {
+        $DOM.append($template);
+        App.initValidation();
+        $.fn.validator.Constructor.INPUT_SELECTOR = 'input.mustCheck';
+
+        App.initForm();
+    },
+    afterEach: function () {
+        $DOM.empty();
+    }
+});
+
+
+QUnit.test('Validation & Custom', function (assert) {
+
+    var validators = $.fn.validator.Constructor.VALIDATORS
+
+    assert.ok(!!validators.hasaddressstreetnumber, "'Has address street number' Appent to Validators Collection")
+    assert.ok(!!validators.datebigger, "'datebigger' Appent to Validators Collection")
+
+    var validator = $('#new_booking_request').data('bs.validator');
+    assert.ok(!!validator, 'validator is Run');
+    assert.ok(validator.$inputs.length === 8, 'validator Active For 8 Inputs ')
+    var date1 = $('#return_at_date');
+    assert.ok(validators.datebigger(date1) === false, 'at_date and Return_at_date are equal togeth3r')
+    var date2 = $('#at_date');
+    var date = new Date();
+    date2.pickadate('picker').set("select", date.setTime(date.getTime() + 3 * 86400000));
+
+    assert.ok(validators.datebigger(date1) != false, 'at_date is bigger than Return_at_date ');
+    var $pickup = $('#pickup');
+    $pickup.val("Lobeckstraße, 10969 Berlin, Germany");
+    var done1 = assert.async();
+    validators.hasaddressstreetnumber($pickup).then(function (error) {
+        assert.ok(!!error, "'Lobeckstraße, 10969 Berlin, Germany' doesn't have street number");
+        done1();
+    });
+    $pickup.val("Lobeckstraße 64, 10969 Berlin, Germany");
+    var done2 = assert.async();
+    validators.hasaddressstreetnumber($pickup).then(function (error) {
+        assert.ok(!error, "'Lobeckstraße 64, 10969 Berlin, Germany' has street number");
+        done2();
+    });
+});
+
+QUnit.test('Form', function (assert) {
+
+    
+    $(".locationpicker input").each(function () {
+        assert.ok(!!$(this).data('placepicker'), 'placepicker is active on ' + this.name);
+    });
+
+    assert.ok(!!$('#return_at_date').pickadate('picker'), 'pickadate is active on return_at_date');
+    assert.ok(!!$('#at_date').pickadate('picker'), 'pickadate is active on at_date');
+    assert.ok(!!$('#return_at_time').pickatime('picker'), 'pickatime is active on return_at_time');
+    assert.ok(!!$('#at_time').pickatime('picker'), 'pickatime is active on at_time');
+});
